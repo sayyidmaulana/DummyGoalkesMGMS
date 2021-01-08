940223bd45317bf23223a07f36e2407bfe93e8e2
Sayyid Maulana Khakul Yakin <sayyidmaulanaofficial@gmail.com>
2021-01-08 14:45:23 +0700

    Initial Commit

---
+++ iCareLogin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iCareLogin
+//
+//  Created by Sayyid Maulana Khakul Yakin on 08/01/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iCareLogin/iCareLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCareLoginApp.swift
+//  iCareLogin
+//
+//  Created by Sayyid Maulana Khakul Yakin on 08/01/21.
+//
+
+import SwiftUI
+
+@main
+struct iCareLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
